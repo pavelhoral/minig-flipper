95de91ccdaf69ec18136167623fa3cc92550d44b
Pavel Horal <horal.pavel@gmail.com>
2013-02-03 04:01:19 +0100

    Layout implemented.

--- src/cz/minig/FlipperActivity.java
+++ src/cz/minig/FlipperActivity.java
@@ -1,22 +1,49 @@
 package cz.minig;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class FlipperActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_flipper);
-	}
+    private int cols = 3;
+    private int rows = 4;
+    private Button[] buttons;
+    private LinearLayout[] rowLayouts;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_flipper, menu);
-		return true;
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Create central layout
+        LinearLayout mainLayout = new LinearLayout(this);
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
+        mainLayout.setGravity(Gravity.FILL);
+        setContentView(mainLayout);
+        // Generate row layouts
+        rowLayouts = new LinearLayout[rows];
+        for (int i = 0; i < rowLayouts.length; i++) {
+            rowLayouts[i] = new LinearLayout(this);
+            rowLayouts[i].setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 1));
+            rowLayouts[i].setGravity(Gravity.FILL);
+            mainLayout.addView(rowLayouts[i]);
+        }
+        // Prepare buttons
+        buttons = new Button[rows * cols];
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i] = new Button(this);
+            buttons[i].setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT, 1));
+            rowLayouts[i / cols].addView(buttons[i]);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_flipper, menu);
+        return true;
+    }
 
 }
